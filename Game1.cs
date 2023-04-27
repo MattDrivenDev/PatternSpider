6b913e9732e0ced45a0f0c86548baedcaa48de4c
Matt Ball <mattdrivedev@gmail.com>
2023-04-27 16:31:20 +0100

    spidey climby (ish - not perfect when rotating)

--- Game1.cs
+++ Game1.cs
@@ -16,6 +16,8 @@ public class Game1 : Game
     private Int32 _spiderFrame;
     private Direction _spiderDirection=Direction.Right;
     private Texture2D _background;
+    private Rectangle _leftWall, _rightWall, _floor;
+    private Boolean _climbing;
 
     public Game1()
     {
@@ -43,35 +45,88 @@ public class Game1 : Game
         _spiderSpriteSheet = Content.Load<Texture2D>("spider128");
         _spiderRectangle = new Rectangle(100, _graphics.PreferredBackBufferHeight-58, 128, 58);
         _spiderFrame=0;
+        _floor=new Rectangle(0, _graphics.PreferredBackBufferHeight-4, _graphics.PreferredBackBufferWidth, 8);
+        _leftWall=new Rectangle(-8, 0, 8, _graphics.PreferredBackBufferHeight);
+        _rightWall=new Rectangle(_graphics.PreferredBackBufferWidth, 0, 8, _graphics.PreferredBackBufferHeight);
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
+        
         // TODO: Add your update logic here
-        if(Keyboard.GetState().IsKeyDown(Keys.Right))
-        {
-            _spiderRectangle.X += 8;
-            _spiderFrame++;
-            _spiderDirection=Direction.Right;
 
-            if(_spiderRectangle.X+128-16>_graphics.PreferredBackBufferWidth){
-                _spiderRectangle.X-=8;
+        if(_spiderRectangle.Intersects(_floor))
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                _spiderRectangle.X += 8;
+                _spiderFrame++;
+                _spiderDirection=Direction.Right;
             }
-        }
-        if(Keyboard.GetState().IsKeyDown(Keys.Left))
-        {
-            _spiderRectangle.X -= 8;
-            _spiderFrame--;
-            _spiderDirection=Direction.Left;
-
-            if(_spiderRectangle.X<=-16){
-                _spiderRectangle.X+=8;
+            if(Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                _spiderRectangle.X -= 8;
+                _spiderFrame--;
+                _spiderDirection=Direction.Left;
             }
         }
 
+        if(_spiderRectangle.Intersects(_leftWall)||_spiderRectangle.Intersects(_rightWall))
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                _spiderRectangle.Y-=8;
+                _spiderFrame--;
+                _spiderDirection=_spiderRectangle.Intersects(_leftWall)
+                    ? Direction.Left:Direction.Right;
+            }
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                _spiderRectangle.Y+=8;
+                _spiderFrame++;
+                _spiderDirection=_spiderRectangle.Intersects(_leftWall)
+                    ? Direction.Right:Direction.Left;
+            }
+        }
+
+        if(!_spiderRectangle.Intersects(_floor))
+        {
+            _climbing=true;
+        }
+
+        if(_climbing&&_spiderRectangle.Intersects(_floor))
+        {
+            _climbing=false;
+        }
+
+        if(_climbing && _spiderRectangle.Intersects(_leftWall)
+            && _spiderRectangle.Y < 0)
+        {
+            _spiderRectangle.Y = 0;
+        }
+        else if(_climbing && _spiderRectangle.Intersects(_rightWall)
+            && _spiderRectangle.Y < 128)
+        {
+            _spiderRectangle.Y = 128;
+        }
+
+        if(_spiderRectangle.Intersects(_floor)&&!_climbing)
+        {
+            _spiderRectangle.Y = _graphics.PreferredBackBufferHeight-58;
+        }
+        
+        if(_spiderRectangle.X<=-16)
+        {
+            _spiderRectangle.X+=8;
+        }
+
+        if(_spiderRectangle.X+128-16>_graphics.PreferredBackBufferWidth)
+        {
+            _spiderRectangle.X-=8;
+        }
         
         if(_spiderFrame>=25)
         {
@@ -106,12 +161,38 @@ public class Game1 : Game
                 Color.White*1f);
         }
 
-        _spriteBatch.Draw(
-            _spiderSpriteSheet, 
-            _spiderRectangle, 
-            new Rectangle(0,58*_spiderFrame,128,58),
-            Color.White, 
-            0, new Vector2(0,0), _spiderDirection==Direction.Left?SpriteEffects.FlipHorizontally:SpriteEffects.None,0);
+        if(!_climbing)
+        {
+            _spriteBatch.Draw(
+                _spiderSpriteSheet, 
+                _spiderRectangle, 
+                new Rectangle(0,58*_spiderFrame,128,58),
+                Color.White, 
+                0, new Vector2(0,0), _spiderDirection==Direction.Left?SpriteEffects.FlipHorizontally:SpriteEffects.None,0);
+        }
+        else
+        {
+            if(_spiderRectangle.Intersects(_leftWall))
+            {                
+                _spriteBatch.Draw(
+                    _spiderSpriteSheet, 
+                    new Rectangle(_spiderRectangle.X+58, _spiderRectangle.Y,_spiderRectangle.Width,_spiderRectangle.Height), 
+                    new Rectangle(0,58*_spiderFrame,128,58),
+                    Color.White, 
+                    MathHelper.ToRadians(90), new Vector2(0,0), _spiderDirection==Direction.Left?SpriteEffects.FlipHorizontally:SpriteEffects.None,0);
+            }
+
+            if(_spiderRectangle.Intersects(_rightWall))
+            {                
+                _spriteBatch.Draw(
+                    _spiderSpriteSheet, 
+                    new Rectangle(_spiderRectangle.X+58, _spiderRectangle.Y,_spiderRectangle.Width,_spiderRectangle.Height), 
+                    new Rectangle(0,58*_spiderFrame,128,58),
+                    Color.White, 
+                    MathHelper.ToRadians(-90), new Vector2(0,0), _spiderDirection==Direction.Left?SpriteEffects.FlipHorizontally:SpriteEffects.None,0);
+            }
+        }
+
         _spriteBatch.End();
 
         base.Draw(gameTime);
